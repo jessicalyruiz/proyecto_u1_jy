7f9f3e35f46c01e38245bbeae7f3678b98ccf54d
Fran-Jos <fjchanataxi@uce.edu.ec>
2022-12-07 19:44:36 -0500

    taller 9 js

---
+++ js/funciones.js
@@ -0,0 +1,15 @@
+function dividir(num1,num2){
+    return num1/num2
+}
+
+function sumar2(num1, num2){
+    return num1+num2
+}
+
+function restar2(n1,n2){
+    return n1-n2
+}
+
+function multiplicar(n1,n2){
+    return n1*n2
+}
